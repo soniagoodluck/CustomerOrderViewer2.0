ec58030794ee2685fbd897a12414b42fc4897c44
Sonia <soniagoodluck@yahoo.co.uk>
2021-03-20 01:44:24 +0000

    Add project files.

---
+++ Repository/CustomerCommand.cs
@@ -0,0 +1,33 @@
+﻿using CustomerOrderViewer2._0.Models;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace CustomerOrderViewer2._0.Repository
+{
+    class CustomerCommand
+    {
+        private string _connectionString;
+
+        public CustomerCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<CustomerModel> GetList()
+        {
+            List<CustomerModel> customers = new List<CustomerModel>();
+
+            var sqlStatement = "Customer_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                customers = connection.Query<CustomerModel>(sqlStatement).ToList();
+            }
+
+            return customers;
+        }
+    }
+}
---
+++ Repository/ItemCommand.cs
@@ -0,0 +1,36 @@
+﻿using CustomerOrderViewer2._0.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CustomerOrderViewer2._0.Repository
+{
+    class ItemCommand
+    {
+            private string _connectionString;
+
+            public ItemCommand(string connectionString)
+            {
+                _connectionString = connectionString;
+            }
+
+            public IList<ItemModel> GetList()
+            {
+                List<ItemModel> items = new List<ItemModel>();
+            
+            //excute the store procedure Getlist
+               var sqlStatement = "Item_GetList";
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    items = connection.Query<ItemModel>(sqlStatement).ToList();
+                }
+
+                return items;
+            }
+        }
+    }
+
